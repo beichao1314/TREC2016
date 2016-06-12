3d404c0fd502c5ecc0f1025d80610e1959e1e170
beichao1314 <334221956@qq.com>
2016-06-12 15:53:46 +0800

    a crawler

--- crawler.py
+++ crawler.py
@@ -8,6 +8,7 @@ Created on Mon May 23 18:26:53 2016
 import tweepy
 from tweepy import OAuthHandler
 import json
+import bz2file
 
 #override tweepy.StreamListener to add logic to on_status
 class MyStreamListener(tweepy.StreamListener):
@@ -16,10 +17,10 @@ class MyStreamListener(tweepy.StreamListener):
         print("status:"+status.text)
     def on_data(self,raw_data):
         data = json.loads(raw_data)
-        d=dict(data)
+        """d=dict(data)
         if len(d.keys())!=1:
-            print(data['text'])
-        print(len(d.keys()))
+            print(data['text'])"""
+        #print(len(d.keys()))
         pass
     
         
@@ -31,11 +32,29 @@ access_token_secret="iYBCfLs3TUT7Yl54ftJoYLuM81Mupy2SbshRBs4N9ffLA"
 
 auth = OAuthHandler(consumer_key,consumer_secret)
 auth.set_access_token(access_token,access_token_secret)
+#api = tweepy.API(auth, proxy='23.83.235.65:NDljMmVhNT@127.0.0.1:1080')
 api = tweepy.API(auth)
 #api = tweepy.API(auth,proxy="")
-
+filepath='E:/TREC/TREC2015-tweetids.txt.bz2'
+#print(api.get_status(622582311184306176))
+with bz2file.open(filepath,'r') as fr:
+    #print(type(lines))
+    for line in fr:
+        strs=str(line,encoding='utf-8')   
+        #print(strs)
+        #print(strs.strip('\n'))
+        try:
+            fw=open('E:/TREC/TREC2015-tweet.txt','a') 
+            tweet=api.get_status(int(strs))
+            print('complete:'+ strs)
+            fw.write(str(tweet)+'\n')
+            fw.close()
+            #print(tweet)
+        except:
+            print('except:')
+            
+        """     
 myStreamListener = MyStreamListener()
 myStream = tweepy.Stream(auth = api.auth, listener=myStreamListener)
 #myStream.filter(track=['python'])
-
-myStream.sample()
+myStream.sample()"""
