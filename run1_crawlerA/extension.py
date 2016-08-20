7691fbc39fd6384703e652993603800c2fb60b4a
Chao Bei <chaobei001@gmail.com>
2016-08-20 21:04:03 +0800

    Update extension.py

--- run1_crawlerA/extension.py
+++ run1_crawlerA/extension.py
@@ -7,8 +7,6 @@ s4 = 'AKvk0/D9XzJuCQA9n/a+TFbqwOFder9xd9Yj/22ivA8'
 s5='r8OUqrE+DW/W4qs8ShfN2ljAU8214AkuksvYy7iMPGk'
 
 def search(search_term):
-    bing_web = PyBingWebSearch(s5, search_term,
-                               web_only=False)  # web_only is optional, but should be true to use your web only quota instead of your all purpose quota
-    first_ten_result = bing_web.search(limit=10, format='json')  # 1-50
-    # second_fifty_result = bing_web.search(limit=50, format='json')  # 51-100
+    bing_web = PyBingWebSearch(s5, search_term, web_only=False)  
+    first_ten_result = bing_web.search(limit=10, format='json')  
     return first_ten_result
